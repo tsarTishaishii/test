45781845f9493538b55830aafe2b33723636c228
Alex <alexeimelkei@gmail.com>
2019-05-11 22:37:20 +0800

    Коммит по задаче

---
+++ Example1/first_task.js
@@ -0,0 +1,15 @@
+"use strict";
+let money = prompt("Ваш бюджет на месяц");
+let time = prompt("Введите дату в формате YYYY-MM-DD") ;
+let appData = {
+    Money: money,
+    timeData: time,
+    expenses: {quection1,quection2 },
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+var quection1 = prompt("Введите обязательную статью расходов в этом месяце");
+var quection2 = prompt("Во сколько обойдется?");
+alert(appData.Money / 30);
+
